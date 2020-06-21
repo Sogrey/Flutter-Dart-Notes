c271d8b318989a447408deb2aea64635dcdf79e1
Sogrey <suzhaoheng2006@163.com>
2020-06-21 22:46:46 +0800

    deploy master

--- FlutterStart/flutter_start/lib/main.dart
+++ FlutterStart/flutter_start/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -49,69 +50,54 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class Item {
+  String text;
+  Color color;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Item(String text, Color color);
+
+  String getText() {
+    return this.text;
   }
+
+  Color getColor() {
+    return this.color;
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final data = [
+    Item('Text', Colors.purple[400]),
+    Item('Icon', Colors.purple[400]),
+    Item('Image', Colors.purple[500]),
+    Item('FlutterLogo', Colors.purple[600]),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Container(
+            child: ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.all(0.0),
+          children: data
+              .map((item){
+                return Container(
+                  alignment: Alignment.centerLeft,
+                  padding:EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  height: 50,
+                  width: 80,
+                  color: item.color,
+                  child: Text(
+                    'item.getText()',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                );
+          } )
+              .toList(),
+        )));
   }
 }
