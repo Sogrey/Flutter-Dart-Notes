bfc804a24166f5cc08f690a0be797d44c46f2660
Sogrey <suzhaoheng2006@163.com>
2020-06-22 00:26:24 +0800

    deploy master

--- FlutterStart/flutter_start/lib/main.dart
+++ FlutterStart/flutter_start/lib/main.dart
@@ -1,48 +1,24 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Start',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Widgets'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -53,8 +29,13 @@ class MyHomePage extends StatefulWidget {
 class Item {
   String text;
   Color color;
+  IconData icon;
 
-  Item(String text, Color color);
+  Item(this.icon, this.text, this.color);
+
+  IconData getIcon() {
+    return this.icon;
+  }
 
   String getText() {
     return this.text;
@@ -66,13 +47,6 @@ class Item {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final data = [
-    Item('Text', Colors.purple[400]),
-    Item('Icon', Colors.purple[400]),
-    Item('Image', Colors.purple[500]),
-    Item('FlutterLogo', Colors.purple[600]),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -80,24 +54,49 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text(widget.title),
         ),
         body: Container(
-            child: ListView(
-          shrinkWrap: true,
-          padding: const EdgeInsets.all(0.0),
-          children: data
-              .map((item){
-                return Container(
-                  alignment: Alignment.centerLeft,
-                  padding:EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  height: 50,
-                  width: 80,
-                  color: item.color,
-                  child: Text(
-                    'item.getText()',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                );
-          } )
-              .toList(),
-        )));
+          child: CustomListView(),
+        ));
+  }
+}
+
+class CustomListView extends StatelessWidget {
+  final data = <Item>[
+    Item(Icons.text_format, 'Text', Colors.purple[300]),
+    Item(Icons.insert_emoticon, 'Icon', Colors.purple[400]),
+    Item(Icons.image, 'Image', Colors.purple[500]),
+    Item(Icons.format_paint, 'Button', Colors.purple[600]),
+    Item(Icons.flag, 'FlutterLogo', Colors.purple[700]),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView(
+        shrinkWrap: true,
+        padding: const EdgeInsets.all(0.0),
+        children: data.map((item) {
+          return Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            height: 50,
+            width: 80,
+            color: item.getColor(),
+            child: Row(
+              children: <Widget>[
+                Icon(
+                  item.getIcon(),
+                  color: Colors.white,
+                ),
+                Spacer(),
+                Text(
+                  item.getText(),
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          );
+        }).toList(),
+      ),
+    );
   }
 }
